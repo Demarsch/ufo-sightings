f59853bb10561e466efd3ae7a582fbef0dbab1da
Andrei Chaplygin <gotacoin@gmail.com>
2018-11-01 22:42:58 -0700

    Datetime validation fix for iOS Safari

--- static/js/report.js
+++ static/js/report.js
@@ -9,4 +9,14 @@ window.addEventListener('load', () => {
             window.location.href = 'forget.html';
         }      
     });
+    //Fix for iOS browsers - input with 'datetime-local' has to have a value
+    //https://stackoverflow.com/questions/43747521/mobile-safari-10-3-1-datetime-local-enter-a-valid-value-error
+    // get the iso time string formatted for usage in an input['type="datetime-local"']
+    var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+    var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0,-1);
+    var localISOTimeWithoutSeconds = localISOTime.slice(0,16);
+    // select the "datetime-local" input to set the default value on
+    var dateInput = document.querySelector('input[type="datetime-local"]');
+    // set it and forget it ;)
+    dateInput.value = localISOTimeWithoutSeconds.slice(0,16);
 });
